0fff0426f82b9c1b0c5186f3216079f4f7ccbfc6
Alena_Tcoi <alena_tsoy@gmail.com>
2017-10-06 14:02:48 +0300

    FirstExample

--- src/com/epam/tcoi/FirstExample.java
+++ src/com/epam/tcoi/FirstExample.java
@@ -2,7 +2,7 @@ package com.epam.tcoi;
 
 public class FirstExample {
     public static void main(String[] args) {
-        int arrayLength = 0;
+        double arrayLength = 0;
         for (String arg : args) arrayLength += arg.length();
         System.out.println(args.length);
         System.out.println(arrayLength/args.length);
