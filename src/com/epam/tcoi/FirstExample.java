eaa820142d7f4760df2cacc6652d052806017e12
Alena_Tcoi <alena_tsoy@gmail.com>
2017-10-06 13:59:59 +0300

    FirstExample

--- src/com/epam/tcoi/FirstExample.java
+++ src/com/epam/tcoi/FirstExample.java
@@ -3,9 +3,7 @@ package com.epam.tcoi;
 public class FirstExample {
     public static void main(String[] args) {
         int arrayLength = 0;
-        for (int i=0; i < args.length; i++){
-            arrayLength += args[1].length();
-        }
+        for (String arg : args) arrayLength += arg.length();
         System.out.println(args.length);
         System.out.println(arrayLength/args.length);
     }
