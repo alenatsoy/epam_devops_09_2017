9ff7f6cafdf79e001b06ddcb8cb151e994cae6b7
Alena_Tcoi <alena_tsoy@gmail.com>
2017-10-16 20:15:26 +0300

    Tasks 3-5

--- src/com/epam/tcoi/Task4.java
+++ src/com/epam/tcoi/Task4.java
@@ -2,11 +2,16 @@ package com.epam.tcoi;
 
 public class Task4 {
     public static void main(String[] args) {
-        if (args.length ==0) return;
-        String min =args [0];
-        for (int i=1; i < args.length; i++) {
-            if (args[i].length() < min.length()) min=args[i];
+        int index = 0;
+        String minStr = args[0];
+        int min = minStr.length();
+        for (int i = 0; i < args.length; i++) {
+            minStr = args[i].replaceAll("(.)(?=.*\\1)", "");
+            if (minStr.length() < min) {
+                min = minStr.length();
+                index = i;
+            };
         }
-        System.out.println(min);
+        System.out.println(args[index]);
     }
 }
